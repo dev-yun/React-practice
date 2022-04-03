ff9dfba397ef91ae6573adaf8a285e576df78451
신윤철 <yunpro2766@gmail.com>
2022-04-04 01:04:06 +0900

    props 공부

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MyHeader from './MyHeader';
 import MyFooter from './MyFooter';
 import Counter from './Counter';
+import Container from './Container';
 
 // jsx 문법 (javascript와 html을 혼용하여 사용)
 // jsx 문법 1. 무조건 닫는 태그가 필요하다. <div></div>같이 닫는 태그가 따로 존재하는 태그나, 셀프 클로징 태그 <image /> 등의 종류가 있다.
@@ -17,28 +18,45 @@ function App() {
   // 우선순위는 인라인 css 작성 방식이 더 높다.
   const style = {
     App : {
-      backgroundColor : "black",
+      backgroundColor : "yellow",
     },
     h2 : {
       color : "red",
     },
     bold_text : {
-      color : "white", 
+      color : "teal", 
     }
   }
 
-  const number = 5;
   // {}에는 변수, 숫자, 문자열, 함수, 식등이 포함될 수 있다. (배열, boolean값등은 렌더링이 안됨)
+  const number = 5;
+
+  const counterProps = {
+    a : 1,
+    b : 2,
+    c : 3,
+    d : 4,
+    e : 5,
+    initialValue : 100,
+  };
+  
+  // 만약 Couter의 count 초기값 0을 부모 컴퍼넌트인 App에서 변경하려면? ==> Props를 사용하면 된다.
+  // Props : 부모 컴퍼넌트에서 자식 컴퍼넌트에게 어떤 값을 이름을 붙혀 전달하는 방법
+  
   return (
-    <div className="App" style={style.App}>
-      <MyHeader />
-        <h2 style={style.h2}> 안녕 리액트 {name}</h2>
-        <b style={style.bold_text}>
-          {number}는 : {number % 2 === 0 ? "짝수" : "홀수"};
-        </b>
-        <Counter />
-      <MyFooter />
-    </div>
+    <Container>
+      <div className="App" style={style.App}>
+        <MyHeader />
+          <h2 style={style.h2}> 안녕 리액트 {name}</h2>
+          <b style={style.bold_text}>
+            {number}는 : {number % 2 === 0 ? "짝수" : "홀수"};
+          </b>
+          {/* <Counter a={1} b={2} c={3} d={4} e={5} initialValue={100}/> */}
+          {/* 위처럼 작성하면 너무 길어져서 counterProps라는 객체를 만들어 사용 */}
+          <Counter {...counterProps} />
+        <MyFooter />
+      </div>
+    </Container>
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,15 @@
 // state(상태)를 사용하면 React와 {useState}를 호출해야함
 // 상태가 변할때마다 re rendering 된다.
 import React,{useState} from 'react';
+import OddEvenResult from './OddEvenResult';
 
-const Counter = () =>{
-
-  // 0에서 +1 -1 씩 상태가 변화하는 코드
-
+// 부모 컴퍼넌트(App)에서 전달한 initialValue를 받아 사용하려면 매개변수로 받아와 사용해야한다.
+// 이때 부모 컴퍼넌트에서 전달받은 값을 객채 형식이고 매개변수.전달받은값이름 형식으로 사용 가능
+const Counter = ({initialValue}) =>{
   // 0번째 인덱스는 상태의 값으로 사용
   // 1번째 인덱스는 상태를 변경시키는 상태변화함수로써 작동
   // useState에 사용한 (0)은 초기값을 의미 (count에 들어감)
-  const [count, setCount ] = useState(0);
+  const [count, setCount ] = useState(initialValue);
 
   const onIncrease = () => {
     setCount(count + 1);
@@ -22,8 +22,13 @@ const Counter = () =>{
       <h2>{count}</h2>
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
+      <OddEvenResult count={count} />
     </div>
   )
+}
+
+Counter.defaultProps = {
+  initialValue:0,
 }
 
 export default Counter;
