c074f9257675478067960b17b912905d59592d00
신윤철 <yunpro2766@gmail.com>
2022-04-03 15:50:32 +0900

    기본 react app을 탐색,수정하며 구성 파악

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+// jsx 문법 (javascript와 html을 혼용하여 사용)
 function App() {
+  let name = "신윤철";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕 리액트
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> 안녕 리액트 {name}</h2>
       </header>
     </div>
   );
 }
 
+// ex_module 시스템 (하나만 export 할 수 있음)
 export default App;
+
+// common js 모듈 시스템과 다름
+// common js module ==> module.exports={} 
