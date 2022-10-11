45c74ac80c24790542df5a1d21b2e6b6e9d1e6d2
dev-yun <yunpro2766@gmail.com>
2022-10-11 17:31:56 +0900

    Feat : 여러 상태를 하나의 state 객체로 관리하는 방법과 이러한 상태 객체를 업데이트하는 두가지 방법 학습

--- code/01-starting-react/src/components/NewExpense/ExpenseForm.js
+++ code/01-starting-react/src/components/NewExpense/ExpenseForm.js
@@ -3,18 +3,59 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   // 기본적으로 입력에 대한 변경 이벤트(event.target.value)는 숫자, date 모두 문자열로 받아오기 때문에 ''로 초기화
+
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  /*
+  위에서 유사한 state가 3번 사용되었는데, 이를 객체로 변환하여 사용할 수 있다.
+  state를 한번에 관리하는 장점이 있지만, 객체 내부의 하나의 state만 변경되어도 모든 state가 변경된다는 점도 존재한다.
+  개발자의 취향에 맞게 선택해서 사용하면 될듯..
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+  */
+
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+
+    /*
+    만약 titleChangeHandler라서 setUserInput에서 title만 변경한다면 오류가 발생한다.
+    setState는 병합의 개념이 아니라 새로운 값으로 교체하는 개념이라서 enteredAmount, enteredDate에는 undefined값이 들어가게 된다. (spread로 객체를 복사하여 추가하고, 업데이트 값으로 오버라이딩을 해야함)
+    
+    setUserInput({
+      // 수동으로 기존의 값을 추가해야한다.
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
+    */
+
+    /*
+    하지만 위의 코드도 문제가 발생한다. (3개의 함수에서 setUserInput을 업데이트하는데, 이렇게 객체를 통해 한번에 값을 관리할 경우 이전의 상태가 최신 상태가 아닐 수 있다.)
+    함수를 통해 객체가 최신의 상태임을 명시하여 사용해야한다.
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+    */
   };
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
 
   return (
