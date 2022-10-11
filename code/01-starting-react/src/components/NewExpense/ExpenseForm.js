b048050228e798d0c77c054e417807767cf46d48
dev-yun <yunpro2766@gmail.com>
2022-10-12 00:28:14 +0900

    Feat : 양방향 바인딩을 통해 form 내부의 입력값 초기화기능 추가

--- code/01-starting-react/src/components/NewExpense/ExpenseForm.js
+++ code/01-starting-react/src/components/NewExpense/ExpenseForm.js
@@ -68,6 +68,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -75,7 +78,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -83,6 +90,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -92,6 +100,7 @@ const ExpenseForm = () => {
             type="date"
             min="2020-01-01"
             max="2024-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
