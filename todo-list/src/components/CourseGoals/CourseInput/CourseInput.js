2c969697b3cac6ac68e16f67377dd852b88d6053
dev-yun <yunpro2766@gmail.com>
2022-11-11 17:56:32 +0900

    Feat: 합성 컴포넌트(Button)를 styled-components로 대체, styled-components로 스타일링 구현

--- todo-list/src/components/CourseGoals/CourseInput/CourseInput.js
+++ todo-list/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,26 +1,73 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const FormControl = styled.div`
+  margin: 0.5rem 0;
 
-  const goalInputChangeHandler = event => {
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background: #ffd7d7;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    // 입력하지 않은 경우 && 공백만 입력한 경우 체크
+    // 이 경우 입력은 되지 않지만 유저는 왜 입력이 안되는지 알지 못함 => 유저에게 안되는 이유와 시각적 표현을 해줘야함
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <FormControl className={!isValid && 'invalid'}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+        {!isValid && <p>문자를 입력해주세요. 공백은 불가능합니다.</p>}
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
