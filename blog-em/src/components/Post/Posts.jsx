c0ef5617d5baaae5d0068306f113ae02e14a0932
dev-yun <yunpro2766@gmail.com>
2022-12-06 20:09:32 +0900

    Feat: react-query 캐시의 개념을 이해하고 자주하는 query-key 실수에 대해 학습 query-key가 같다면 같은 query로 인식하여 refetch가 발생하지 않음

---
+++ blog-em/src/components/Post/Posts.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+import { PostDetail } from './PostDetail';
+const MAX_POST_PAGE = 10;
+
+async function fetchPosts(page) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${MAX_POST_PAGE}&_page=${page}`
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const { data, isLoading, isError } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
+
+  if (isLoading) return <h3>로딩 중 입니다.</h3>;
+  if (isError) return <h3>서버에 에러가 있습니다.</h3>;
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            currentPage >= 2 &&
+              setCurrentPage((previousPage) => previousPage - 1);
+          }}
+        >
+          Previous page
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= MAX_POST_PAGE}
+          onClick={() => {
+            setCurrentPage((previousPage) => previousPage + 1);
+          }}
+        >
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
